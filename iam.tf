015686614958994a50f34822774aab2b097d5fe6
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-06-10 20:01:20 +0530

    modified the eks.tf file

--- iam.tf
+++ iam.tf
@@ -23,19 +23,19 @@ resource "aws_iam_role" "eks_cluster_role" {
 #   role       = aws_iam_role.eks_cluster_role.arn
 # }
 
-# resource "aws_iam_role" "eks_node_role" {
-#   name = "eks-node-role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Action = "sts:AssumeRole"
-#       Effect = "Allow"
-#       Principal = {
-#         Service = "ec2.amazonaws.com"
-#       }
-#     }]
-#   })
-# }
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
 
 
 
