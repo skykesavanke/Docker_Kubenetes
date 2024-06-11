b557edee713bd84d55b7b3845a62a5960e58c140
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-06-11 10:27:13 +0530

    modified the eks.tf file

--- eks.tf
+++ eks.tf
@@ -28,7 +28,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 
 
 resource "aws_eks_node_group" "eks_node_grp" {
-  cluster_name    = var.aws_eks_cluster.eks_node_grp.name
+  cluster_name    = var.aws_eks_cluster
   node_group_name = var.aws_eks_node_group
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = aws_subnet.main[*].id
