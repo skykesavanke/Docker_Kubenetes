690e00f37f6b455f579e6a98530b702cd44286f2
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-06-11 21:25:18 +0530

    added ingress

--- eks.tf
+++ eks.tf
@@ -59,6 +59,12 @@ instance_types = ["t3.medium"]
 }
 resource "aws_security_group" "cluster_security_group" {
   vpc_id = aws_vpc.main.id
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 443
