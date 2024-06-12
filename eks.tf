c22738c1c46443f969adf43cd1348f6145014810
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-06-12 10:06:50 +0530

    added ingress

--- eks.tf
+++ eks.tf
@@ -41,13 +41,23 @@ resource "aws_eks_node_group" "eks_node_grp" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = var.aws_eks_node_group
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = aws_subnet.main[*].id
+  subnet_ids  =aws_subnet.main[*].id
 
   scaling_config {
-    desired_size = 2
+    desired_size = 1
     max_size     = 2
     min_size     = 1
   }
+    update_config {
+    max_unavailable = 1
+  }
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_container_registry_read_only,
+  ]
+
+
 
 instance_types = ["t3.medium"]
 
